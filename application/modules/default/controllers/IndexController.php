23b987864f537869ca92dd0e66fa2126a7f9560c
newLoki <newloki91@googlemail.com>
2011-07-17 16:51:00 +0200

    Add newest scores as table to index action (and also add formated output in view)

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -43,8 +43,28 @@ class IndexController extends BaseController
      * @return           void
      *
      */
-    public function indexAction() {
-        
+    public function indexAction()
+    {
+
+        /** @var $scoresRepo Custom_Repository_ScoreRepository */
+        $scoresRepo = $this->_em->getRepository('Custom_Entity_Score');
+        //new Custom_Repository_ScoreRepository($this->_em, 'Custom_Entity_Score');
+        $scores = $scoresRepo->getNewestScores(50);
+
+        $scoresForView = array();
+
+        /** @var $score Custom_Entity_Score */
+        foreach($scores as $score) {
+            $scoresForView[$score->getId()] = array(
+                'id' => $score->getId(),
+                'score' => $score->getScore(),
+                'created_at' => $score->getCreatedAt(),
+                'changed_at' => $score->getChangedAt(),
+                'created_by' => $score->getCreatedBy()->getId()
+            );
+        }
+
+        $this->view->tableContent = $scoresForView;
     }
 
 }
